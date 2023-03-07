f0bf52d47875c6aaf25fac7bdf08cd5725f5fc87
KhanKou <huynh.quockhang920@gmail.com>
2023-03-07 07:54:07 +0700

    Front-end Update #9

---
+++ js/im-ani-src-video.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var urlGetToken = "https://api.karyl.live/v1/get_token"
+    axios.get(urlGetToken).then(token => {
+        var data = JSON.stringify({
+            query: `query {
+    sourceById(id: "e676e5b9-4073-45e1-8e6b-12d265a5874e") {
+        providerId
+        id
+        url
+    }
+       }`,
+            variables: {}
+        });
+
+        var config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://api.karyl.live/graphql',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
+})
